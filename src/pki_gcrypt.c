fec6cf3b4a7f0fbd55bc7cccace3e4601a2f12c2
openvela-robot <openvela-robot@xiaomi.com>
2019-12-11 11:28:33 +0800

    kex: Avoid always-false comparisons as reported by csbuild
    /builds/jjelen/libssh-mirror/src/kex.c:360:17: warning: comparison of unsigned expression < 0 is always false [-Wtype-limits] <--[cc]
      360 |         if (len < 0) {
          |                 ^
    /builds/jjelen/libssh-mirror/src/kex.c:372:17: warning: comparison of unsigned expression < 0 is always false [-Wtype-limits] <--[cc]
      372 |         if (len < 0) {
          |                 ^
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>
    (cherry picked from commit e2841908fbe5a236ab075ac0a13c26af96f5fe64)

--- src/pki_gcrypt.c
+++ src/pki_gcrypt.c
@@ -315,7 +315,7 @@ static int privatekey_decrypt(int algo, int mode, unsigned int key_len,
   if (gcry_cipher_open(&cipher, algo, mode, 0)
       || gcry_cipher_setkey(cipher, key, key_len)
       || gcry_cipher_setiv(cipher, iv, iv_len)
-      || (tmp = calloc(ssh_buffer_get_len(data), sizeof(unsigned char))) == NULL
+      || (tmp = calloc(ssh_buffer_get_len(data), sizeof(char))) == NULL
       || gcry_cipher_decrypt(cipher, tmp, ssh_buffer_get_len(data),
                        ssh_buffer_get(data), ssh_buffer_get_len(data))) {
     gcry_cipher_close(cipher);
@@ -937,8 +937,6 @@ ssh_string pki_private_key_to_pem(const ssh_key key,
     (void) passphrase;
     (void) auth_fn;
     (void) auth_data;
-
-    SSH_LOG(SSH_LOG_WARN, "PEM export not supported by gcrypt backend!");
 
     return NULL;
 }
@@ -2459,13 +2457,4 @@ int pki_verify_data_signature(ssh_signature signature,
     return SSH_OK;
 }
 
-int pki_uri_import(const char *uri_name, ssh_key *key, enum ssh_key_e key_type)
-{
-    (void) uri_name;
-    (void) key;
-    (void) key_type;
-    SSH_LOG(SSH_LOG_WARN,
-            "gcrypt does not support PKCS #11");
-    return SSH_ERROR;
-}
 #endif /* HAVE_LIBGCRYPT */
