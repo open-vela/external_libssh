94e1262e1554f3b6775f685d34a0ff4fc34682a9
openvela-robot <openvela-robot@xiaomi.com>
2020-06-19 11:59:33 +0800

    tests: Add test for CVE-2019-14889
    The test checks if a command appended to the file path is not executed.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>
    (cherry picked from commit 1694606e12d8950b003ff86248883732ef05e00c)

--- src/channels.c
+++ src/channels.c
@@ -544,7 +544,7 @@ SSH_PACKET_CALLBACK(channel_rcv_data){
   len = ssh_string_len(str);
 
   SSH_LOG(SSH_LOG_PACKET,
-      "Channel receiving %zu bytes data in %d (local win=%d remote win=%d)",
+      "Channel receiving %" PRIdS " bytes data in %d (local win=%d remote win=%d)",
       len,
       is_stderr,
       channel->local_window,
@@ -553,7 +553,7 @@ SSH_PACKET_CALLBACK(channel_rcv_data){
   /* What shall we do in this case? Let's accept it anyway */
   if (len > channel->local_window) {
     SSH_LOG(SSH_LOG_RARE,
-        "Data packet too big for our window(%zu vs %d)",
+        "Data packet too big for our window(%" PRIdS " vs %d)",
         len,
         channel->local_window);
   }
@@ -2932,15 +2932,14 @@ int ssh_channel_read_timeout(ssh_channel channel,
   if (session->session_state == SSH_SESSION_STATE_ERROR) {
       return SSH_ERROR;
   }
-  /* If the server closed the channel properly, there is nothing to do */
-  if (channel->remote_eof && ssh_buffer_get_len(stdbuf) == 0) {
-      return 0;
-  }
   if (channel->state == SSH_CHANNEL_STATE_CLOSED) {
       ssh_set_error(session,
                     SSH_FATAL,
                     "Remote channel is closed.");
       return SSH_ERROR;
+  }
+  if (channel->remote_eof && ssh_buffer_get_len(stdbuf) == 0) {
+    return 0;
   }
   len = ssh_buffer_get_len(stdbuf);
   /* Read count bytes if len is greater, everything otherwise */
