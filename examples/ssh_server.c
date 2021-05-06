67582057659c9e7ef679dfbe602da8026d780a4a
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-05-06 23:29:50 +0800

    examples/ssh_server: Free the resource in the failure path
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- examples/ssh_server.c
+++ examples/ssh_server.c
@@ -722,6 +722,7 @@ int main(int argc, char **argv) {
     sshbind = ssh_bind_new();
     if (sshbind == NULL) {
         fprintf(stderr, "ssh_bind_new failed\n");
+        ssh_finalize();
         return 1;
     }
 
@@ -736,6 +737,8 @@ int main(int argc, char **argv) {
 
     if(ssh_bind_listen(sshbind) < 0) {
         fprintf(stderr, "%s\n", ssh_get_error(sshbind));
+        ssh_bind_free(sshbind);
+        ssh_finalize();
         return 1;
     }
 
