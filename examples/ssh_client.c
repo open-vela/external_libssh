aa9285f201ac2d02f3f8548376982619e9faa25e
openvela-robot <openvela-robot@xiaomi.com>
2021-05-09 07:47:58 +0800

    packet: Change the last argument of ssh_packet_encrypt to uint32_t
    to match the implemntation in packet_crypt.c
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Sahana Prasad <sahana@redhat.com>
    Change-Id: Ib76c3585f67dae22ed0f1dfc10dadcd03c762032

--- examples/ssh_client.c
+++ examples/ssh_client.c
@@ -44,9 +44,10 @@
 #include "examples_common.h"
 #define MAXCMD 10
 
-static char *host;
-static char *user;
+static char *host = NULL;
+static char *user = NULL;
 static char *cmds[MAXCMD];
+static char *config_file = NULL;
 static struct termios terminal;
 
 static char *pcap_file = NULL;
@@ -81,7 +82,7 @@ static void add_cmd(char *cmd)
         return;
     }
 
-    cmds[n] = cmd;
+    cmds[n] = strdup(cmd);
 }
 
 static void usage(void)
@@ -94,6 +95,7 @@ static void usage(void)
             "  -p port : connect to port\n"
             "  -d : use DSS to verify host public key\n"
             "  -r : use RSA to verify host public key\n"
+            "  -F file : parse configuration file instead of default one\n"
 #ifdef WITH_PCAP
             "  -P file : create a pcap debugging file\n"
 #endif
@@ -110,10 +112,13 @@ static int opts(int argc, char **argv)
 {
     int i;
 
-    while((i = getopt(argc,argv,"T:P:")) != -1) {
+    while((i = getopt(argc,argv,"T:P:F:")) != -1) {
         switch(i){
         case 'P':
             pcap_file = optarg;
+            break;
+        case 'F':
+            config_file = optarg;
             break;
 #ifndef _WIN32
         case 'T':
@@ -122,7 +127,7 @@ static int opts(int argc, char **argv)
 #endif
         default:
             fprintf(stderr, "Unknown option %c\n", optopt);
-            return -1;
+            usage();
         }
     }
     if (optind < argc) {
@@ -134,7 +139,7 @@ static int opts(int argc, char **argv)
     }
 
     if (host == NULL) {
-        return -1;
+        usage();
     }
 
     return 0;
@@ -169,25 +174,22 @@ static void do_exit(int i)
     exit(0);
 }
 
+static ssh_channel chan;
 static int signal_delayed = 0;
 
-#ifdef SIGWINCH
 static void sigwindowchanged(int i)
 {
     (void) i;
     signal_delayed = 1;
 }
-#endif
 
 static void setsignal(void)
 {
-#ifdef SIGWINCH
     signal(SIGWINCH, sigwindowchanged);
-#endif
     signal_delayed = 0;
 }
 
-static void sizechanged(ssh_channel chan)
+static void sizechanged(void)
 {
     struct winsize win = {
         .ws_row = 0,
@@ -225,7 +227,7 @@ static void select_loop(ssh_session session,ssh_channel channel)
 
     while (ssh_channel_is_open(channel)) {
         if (signal_delayed) {
-            sizechanged(channel);
+            sizechanged();
         }
         rc = ssh_event_dopoll(event, 60000);
         if (rc == SSH_ERROR) {
@@ -265,9 +267,10 @@ static void shell(ssh_session session)
         ssh_channel_free(channel);
         return;
     }
+    chan = channel;
     if (interactive) {
         ssh_channel_request_pty(channel);
-        sizechanged(channel);
+        sizechanged();
     }
 
     if (ssh_channel_request_shell(channel)) {
@@ -328,7 +331,7 @@ static int client(ssh_session session)
             return -1;
         }
     }
-    if (ssh_options_set(session, SSH_OPTIONS_HOST ,host) < 0) {
+    if (ssh_options_set(session, SSH_OPTIONS_HOST, host) < 0) {
         return -1;
     }
     if (proxycommand != NULL) {
@@ -336,7 +339,13 @@ static int client(ssh_session session)
             return -1;
         }
     }
-    ssh_options_parse_config(session, NULL);
+    /* Parse configuration file if specified: The command-line options will
+     * overwrite items loaded from configuration file */
+    if (config_file != NULL) {
+        ssh_options_parse_config(session, config_file);
+    } else {
+        ssh_options_parse_config(session, NULL);
+    }
 
     if (ssh_connect(session)) {
         fprintf(stderr, "Connection failed : %s\n", ssh_get_error(session));
@@ -400,20 +409,18 @@ int main(int argc, char **argv)
 {
     ssh_session session;
 
-    ssh_init();
     session = ssh_new();
 
     ssh_callbacks_init(&cb);
     ssh_set_callbacks(session,&cb);
 
-    if (ssh_options_getopt(session, &argc, argv) || opts(argc, argv)) {
+    if (ssh_options_getopt(session, &argc, argv)) {
         fprintf(stderr,
                 "Error parsing command line: %s\n",
                 ssh_get_error(session));
-        ssh_free(session);
-        ssh_finalize();
         usage();
     }
+    opts(argc, argv);
     signal(SIGTERM, do_exit);
 
     set_pcap(session);
