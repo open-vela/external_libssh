539146f41279e22306b0bc4d4c0950c6d002d451
openvela-robot <openvela-robot@xiaomi.com>
2022-04-18 16:39:35 +0800

    trivial fix: Fixes some spelling errors
    Signed-off-by: Dhanuka Warusadura <csx@tuta.io>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/dh-gex.c
+++ src/dh-gex.c
@@ -263,6 +263,8 @@ static SSH_PACKET_CALLBACK(ssh_packet_client_dhgex_reply)
         bignum_safe_free(server_pubkey);
         goto error;
     }
+    /* The ownership was passed to the crypto structure */
+    server_pubkey = NULL;
 
     rc = ssh_dh_import_next_pubkey_blob(session, pubkey_blob);
     SSH_STRING_FREE(pubkey_blob);
@@ -293,6 +295,7 @@ static SSH_PACKET_CALLBACK(ssh_packet_client_dhgex_reply)
 
     return SSH_PACKET_USED;
 error:
+    SSH_STRING_FREE(pubkey_blob);
     ssh_dh_cleanup(session->next_crypto);
     session->session_state = SSH_SESSION_STATE_ERROR;
 
@@ -467,7 +470,7 @@ static int ssh_retrieve_dhgroup_file(FILE *moduli,
                 line);
     } else {
         SSH_LOG(SSH_LOG_WARNING,
-                "No moduli found for [%"PRIu32":%"PRIu32":%"PRIu32"]",
+                "No moduli found for [%u:%u:%u]",
                 pmin,
                 pn,
                 pmax);
@@ -486,7 +489,8 @@ static int ssh_retrieve_dhgroup_file(FILE *moduli,
  * @param[out] g generator
  * @return SSH_OK on success, SSH_ERROR otherwise.
  */
-static int ssh_retrieve_dhgroup(uint32_t pmin,
+static int ssh_retrieve_dhgroup(char *moduli_file,
+                                uint32_t pmin,
                                 uint32_t pn,
                                 uint32_t pmax,
                                 size_t *size,
@@ -505,7 +509,11 @@ static int ssh_retrieve_dhgroup(uint32_t pmin,
         return ssh_fallback_group(pmax, p, g);
     }
 
-    moduli = fopen(MODULI_FILE, "r");
+    if (moduli_file != NULL)
+        moduli = fopen(moduli_file, "r");
+    else
+        moduli = fopen(MODULI_FILE, "r");
+
     if (moduli == NULL) {
         SSH_LOG(SSH_LOG_WARNING,
                 "Unable to open moduli file: %s",
@@ -602,12 +610,12 @@ static SSH_PACKET_CALLBACK(ssh_packet_server_dhgex_request)
         ssh_set_error_invalid(session);
         goto error;
     }
-    SSH_LOG(SSH_LOG_INFO, "dh-gex: DHGEX_REQUEST[%"PRIu32":%"PRIu32":%"PRIu32"]", pmin, pn, pmax);
+    SSH_LOG(SSH_LOG_INFO, "dh-gex: DHGEX_REQUEST[%u:%u:%u]", pmin, pn, pmax);
 
     if (pmin > pn || pn > pmax || pn > DH_PMAX || pmax < DH_PMIN) {
         ssh_set_error(session,
                       SSH_FATAL,
-                      "Invalid dh-gex arguments [%"PRIu32":%"PRIu32":%"PRIu32"]",
+                      "Invalid dh-gex arguments [%u:%u:%u]",
                       pmin,
                       pn,
                       pmax);
@@ -624,7 +632,8 @@ static SSH_PACKET_CALLBACK(ssh_packet_server_dhgex_request)
             pn = pmin;
         }
     }
-    rc = ssh_retrieve_dhgroup(pmin,
+    rc = ssh_retrieve_dhgroup(session->opts.moduli_file,
+                              pmin,
                               pn,
                               pmax,
                               &size,
@@ -633,7 +642,7 @@ static SSH_PACKET_CALLBACK(ssh_packet_server_dhgex_request)
     if (rc == SSH_ERROR) {
         ssh_set_error(session,
                       SSH_FATAL,
-                      "Couldn't find DH group for [%"PRIu32":%"PRIu32":%"PRIu32"]",
+                      "Couldn't find DH group for [%u:%u:%u]",
                       pmin,
                       pn,
                       pmax);
