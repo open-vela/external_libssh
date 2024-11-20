libssh/libssh-mirror

Replace the hardcode max path length with PATH_MAX
Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
Reviewed-by: Jakub Jelen <jjelen@redhat.com>
Change-Id: Icb1d36b48a759ec11dbaa4c09a39037a80ab0f85

src/auth.c
@@ -1103,7 +1103,7 @@ int ssh_userauth_publickey_auto(ssh_session session,
 
     while (state->it != NULL) {
         const char *privkey_file = state->it->data;
-        char pubkey_file[1024] = {0};
+        char pubkey_file[PATH_MAX] = {0};
 
         if (state->state == SSH_AUTH_AUTO_STATE_PUBKEY) {
             SSH_LOG(SSH_LOG_DEBUG,

open-vela/external_libssh

Change-Id: Ie0689b9d4ffa561b708876ab699cd47da424eb36

src/auth.c
@@ -1054,7 +1054,8 @@ int ssh_userauth_publickey_auto(ssh_session session,
 
     while (state->it != NULL) {
         const char *privkey_file = state->it->data;
-        char pubkey_file[1024] = {0};
+        char pubkey_file[MAX_PATH] = {0};
+
         if (state->state == SSH_AUTH_AUTO_STATE_PUBKEY) {
             SSH_LOG(SSH_LOG_DEBUG,
                     "Trying to authenticate with %s", privkey_file);